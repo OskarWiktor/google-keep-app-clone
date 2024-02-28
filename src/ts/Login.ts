fd1c0769ac47d82d285cc7ec4a43d265a8f16191
Oskar Wiktor <oskar.wiktor3@wp.pl>
2024-02-28 22:25:43 +0100

    fix: scss clean-up + Login.ts cleaner code

--- src/ts/Login.ts
+++ src/ts/Login.ts
@@ -1,78 +1,50 @@
-import {getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth";
+import {getAuth, GoogleAuthProvider, signInWithPopup, User} from "firebase/auth";
 
 class Login {
-  protected loginWrapper: HTMLElement = document.getElementById(
-    "login-wrapper"
-  ) as HTMLElement;
-  protected loginOpen: HTMLElement = document.getElementById(
-    "login-open"
-  ) as HTMLElement;
-  protected loginCloseIcon: HTMLElement = document.getElementById(
-    "login-close"
-  ) as HTMLElement;
-
-  protected signInButton: HTMLElement = document.getElementById(
-    "sign-in"
-  ) as HTMLElement;
-  protected signOutButton: HTMLElement = document.getElementById(
-    "sign-out"
-  ) as HTMLElement;
-
+  protected loginWrapper: HTMLElement = document.getElementById( "login-wrapper" ) as HTMLElement;
+  protected loginOpen: HTMLElement = document.getElementById( "login-open" ) as HTMLElement;
+  protected loginCloseIcon: HTMLElement = document.getElementById( "login-close" ) as HTMLElement;
+  protected signInButton: HTMLElement = document.getElementById( "sign-in" ) as HTMLElement;
   protected auth = getAuth();
   protected provider = new GoogleAuthProvider();
 
-  protected userEmail: HTMLElement = document.querySelector(
-    ".email--email"
-  ) as HTMLElement;
-  protected userWelcome: HTMLElement = document.querySelector(
-    ".account-edit--welcome"
-  ) as HTMLElement;
-  protected userPhoto: HTMLImageElement = document.querySelector(
-    ".account--bg"
-  ) as HTMLImageElement;
-  protected userPhotoLoginOpen: HTMLImageElement = document.querySelector(
-    ".account-edit--photo__wrapper"
-  ) as HTMLImageElement;
   constructor() {
-    this.loginOpen.addEventListener("click", this.handleLoginOpen.bind(this));
-    this.loginCloseIcon.addEventListener(
-      "click",
-      this.handleLoginClose.bind(this)
-    );
-    document.addEventListener(
-      "click",
-      this.handleDocumentLoginClose.bind(this)
-    );
-    this.signInButton.addEventListener("click", this.handleSignIn.bind(this));
+    this.initEvents();
   }
-  handleLoginOpen() {
-    this.loginWrapper.classList.toggle("active");
+
+  protected initEvents(): void {
+    this.loginOpen.addEventListener( "click", this.handleLoginOpen );
+    this.loginCloseIcon.addEventListener( "click", this.handleLoginClose );
+    document.addEventListener( "click", this.handleDocumentLoginClose );
+    this.signInButton.addEventListener( "click", this.handleSignIn ); 
   }
-  handleLoginClose() {
-    this.loginWrapper.classList.remove("active");
-  }
-  handleDocumentLoginClose(event: Event) {
-    if (
-      !this.loginWrapper.contains(event.target as Node) &&
-      !this.loginOpen.contains(event.target as Node)
-    ) {
+
+  protected handleLoginOpen = (): void => { this.loginWrapper.classList.toggle("active") };
+  protected handleLoginClose = (): void => { this.loginWrapper.classList.remove("active") };
+  protected handleDocumentLoginClose = (event: Event): void => {
+    if (!this.loginWrapper.contains(event.target as Node) && !this.loginOpen.contains(event.target as Node)) { 
       this.handleLoginClose();
     }
-  }
-  handleSignIn() {
-    signInWithPopup(this.auth, this.provider)
-      .then((result) => {
-        const user = result.user;
-        console.log(user);
+  };
+  protected handleSignIn = async (): Promise<void> => {
+    try {
+      const result = await signInWithPopup(this.auth, this.provider);
+      const user = result.user;
+      this.updateUserInterface(user);
+    } catch (error) {
+      alert(`Nie można się zalogować: ${error}`)
+    }
+  };
+  protected updateUserInterface(user: User): void {
+    const userEmail: HTMLElement = document.querySelector( ".email--email" ) as HTMLElement;
+    const userWelcome: HTMLElement = document.querySelector( ".account-edit--welcome" ) as HTMLElement;
+    const userPhoto: HTMLImageElement = document.querySelector( ".account--bg" ) as HTMLImageElement;
+    const userPhotoLoginOpen: HTMLImageElement = document.querySelector( ".account-edit--photo__wrapper" ) as HTMLImageElement;
 
-        this.userEmail.textContent = `${user.email}`;
-        this.userWelcome.textContent = `Witaj ${user.displayName},`;
-        this.userPhoto.style.backgroundImage = `url(${user.photoURL})`;
-        this.userPhotoLoginOpen.style.backgroundImage = `url(${user.photoURL})`;
-      })
-      .catch((error) => {
-        console.log("Error signing in", error);
-      });
+    userEmail.textContent = `${user.email}`;
+    userWelcome.textContent = `Witaj ${user.displayName},`;
+    userPhoto.style.backgroundImage = `url(${user.photoURL})`;
+    userPhotoLoginOpen.style.backgroundImage = `url(${user.photoURL})`;
   }
 }
 const login = new Login();
